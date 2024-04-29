6343496f9bb7b13137591c6c14b415e1cb9791d8
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-04-29 23:33:22 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -4,8 +4,10 @@ import os
 from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit, usertype, addpre
+from config import Config
+
 ADMIN = int(os.environ.get("ADMIN", 1484670284))
-log_channel = int(os.environ.get("LOG_CHANNEL", "-1002124243193"))
+log_channel = Config.LOG_CHANNEL
 
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["warn"]))
