c904172832a1db20b50ecf36a208010eccb104e1
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-04-29 23:30:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,13 @@ class Config(object):
   # database config
     DB_NAME = os.environ.get("DB_NAME","Agsmod")     
     DB_URL  = os.environ.get("DB_URL","")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+
+class Txt(object):
+  PROGRESS_BAR = """\n
+╭━━━━❰ᴏɴ ꜰɪʀᴇ 🔥❱━➣
+┣⪼ <b>🗃️ Sɪᴢᴇ:</b> {1} | {2}
+┣⪼ <b>⏳️ Dᴏɴᴇ :</b> {0}%
+┣⪼ <b>🚀 Sᴩᴇᴇᴅ:</b> {3}/s
+┣⪼ <b>⏰️ Eᴛᴀ:</b> {4}
+╰━━━━━━━━━━━━━━━➣"""
