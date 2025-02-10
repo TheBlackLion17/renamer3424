1eee98cff667063ad4ffb204ec7481e4fb7d03bb
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-02-11 00:58:14 +0530

    2.01

--- bot.py
+++ bot.py
@@ -1,45 +1,60 @@
-import asyncio
-from pyrogram import Client, compose,idle
 import os
-from datetime import datetime, timedelta
-monitoredSince = datetime.now() 
+from datetime import datetime
+from pytz import timezone
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from config import Config
+from aiohttp import web
+from route import web_server
+import pyrogram.utils
+import pyromod
 
-monitoredSince -= timedelta(seconds=5)
-
-print(monitoredSince)
-
-from plugins.cb_data import app as Client2
-
-TOKEN = os.environ.get("TOKEN", "6041996472:AAECug5QJXvYuKfxf5-nGfC_gUAz_VzZ6OM")
-
-API_ID = int(os.environ.get("API_ID", "22373721"))
-
-API_HASH = os.environ.get("API_HASH", "6bf8fcaa229c4948941c501a0a5c027c")
-
-STRING = os.environ.get("STRING", "BQFVZVkAThwaD5z3Y6BA1qihdkwjNugS00S0Rfcsp5Q6iYIrPv6y8qPcL6d6CkV4T_GEF-4NlFjmo9H8z9aMDsExWvnBL_v9TF6n8F1WthWzD3WqLtwhWjjUMcZuO2nmXe51EwwOI_vxkhYfA_KH1OXmLVYQE3U0uuzze1IVDZvP2mwebpmE-SZA9yceE49RNccpF002dzE3I212Hkn8lwGLyMR9GvD8kQs_lFNi6FGirS0rcPq5IrMzoysaZyv8Lb4illRmJIXbliCU9xQDAE7uV8DuR1mu-kNIaZam5Bs8zq-8hhNM0LZohaWTA9QdQcFwIn_F638PGtq19uxB1JxdZi8XBgAAAAGf1B37AA")
+pyrogram.utils.MIN_CHAT_ID = -999999999999
+pyrogram.utils.MIN_CHANNEL_ID = -1009999999999
 
 
+class Bot(Client):
 
-bot = Client(
+    def __init__(self):
+        super().__init__(
+            name="renamer",
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN,
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=15,
+        )
 
-           "Renamer",
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.mention = me.mention
+        self.username = me.username  
+        self.uptime = Config.BOT_UPTIME     
+        if Config.WEBHOOK:
+            app = web.AppRunner(await web_server())
+            await app.setup()
+            PORT = int(os.environ.get("PORT", 8000))  # Use port 8000 or env PORT
+            await web.TCPSite(app, "0.0.0.0", PORT).start()
+        print(f"{me.first_name} Is Started.....✨️")
+        for id in Config.ADMIN:
+            try: 
+                await self.send_message(id, f"**{me.first_name} Is Started...**")                                
+            except Exception as e:
+                print(f"Error sending message to admin {id}: {e}")
+        
+        if Config.LOG_CHANNEL:
+            try:
+                curr = datetime.now(timezone("Asia/Kolkata"))
+                date = curr.strftime('%d %B, %Y')
+                time = curr.strftime('%I:%M:%S %p')
+                await self.send_message(Config.LOG_CHANNEL, f"**{me.mention} Is Restarted !!**\n\n📅 Date : `{date}`\n⏰ Time : `{time}`\n🌐 Timezone : `Asia/Kolkata`\n\n🉐 Version : `v{__version__} (Layer {layer})`</b>")                                
+            except Exception as e:
+                print(f"Error sending message to LOG_CHANNEL: {e}")
 
-           bot_token=TOKEN,
+    async def stop(self):
+        await super().stop()
+        print(f"{self.mention} is stopped.")
 
-           api_id=API_ID,
-
-           api_hash=API_HASH,
-
-           plugins=dict(root='plugins'))
-           
-
-if STRING:
-    apps = [Client2,bot]
-    for app in apps:
-        app.start()
-    idle()
-    for app in apps:
-        app.stop()
-    
-else:
-    bot.run()
+Bot().run()
