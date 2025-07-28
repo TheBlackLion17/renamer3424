ce7eb31cfb8abee7d9c437ee50ccf14a5679e4d7
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-07-28 21:44:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-#Thank you LazyDeveloper for helping me in this journey !
-#Must Subscribe On YouTube @LazyDeveloperr 
+# Thank you LazyDeveloper for helping me in this journey!
+# Must Subscribe On YouTube @LazyDeveloperr
 
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '@LazyDeveloper'
-
+def home():
+    return '@TheBlackLion17'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8080)  # Added host/port for deployment compatibility
